5c0d838a7279861f8f0cbeb6e91bea40bbc83700
cssunaa <cssunaa@ecelvd712.ece.local>
2025-06-07 21:12:29 +0800

    [design][lib] added basic dff modules

---
+++ verilog/lib/DFFE.v
@@ -0,0 +1,21 @@
+module DFFR(
+    clk,
+    d,
+    en,
+    q,
+);
+
+parameter WIDTH = 1;
+
+input wire clk;
+input wire en;
+input wire [WIDTH-1:0]d;
+output reg [WIDTH-1:0]q;
+
+always@(posedge clk or negedge rst_n)begin
+    if(en) begin
+        q <= d;
+    end
+end
+
+endmodule
---
+++ verilog/lib/DFFR.v
@@ -0,0 +1,24 @@
+module DFFR(
+    clk,
+    rst_n,
+    d,
+    q,
+);
+
+parameter WIDTH = 1;
+
+input wire clk;
+input wire rst_n;
+input wire [WIDTH-1:0]d;
+output reg [WIDTH-1:0]q;
+
+always@(posedge clk or negedge rst_n)begin
+    if(~rst_n)begin
+        q <= {WIDTH{1'b0}};
+    end
+    else begin
+        q <= d;
+    end
+end
+
+endmodule
