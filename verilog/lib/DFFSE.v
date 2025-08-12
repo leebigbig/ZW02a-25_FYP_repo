306ce919bd178d6875ecb45db075de08f44998c9
AndySCS <cssunaa@connect.ust.hk>
2025-08-12 23:48:21 +0800

    [design][lib] added new dff module

---
+++ verilog/lib/DFFSE.v
@@ -0,0 +1,26 @@
+module DFFR(
+    clk,
+    rst_n,
+    d,
+    en,
+    q,
+);
+
+parameter WIDTH = 1;
+
+input wire clk;
+input wire rst_n;
+input wire en;
+input wire [WIDTH-1:0]d;
+output reg [WIDTH-1:0]q;
+
+always@(posedge clk or negedge rst_n)begin
+    if(~rst_n)begin
+        q <= {WIDTH{1'b1}};
+    end
+    else if(en) begin
+        q <= d;
+    end
+end
+
+endmodule
