d2aa4b669441db9d48176b3022418b4b782a6e8a
AndySCS <cssunaa@connect.ust.hk>
2025-09-03 23:16:50 +0800

    [script][git] added commit script

---
+++ script/git_commit.py
@@ -0,0 +1,21 @@
+import subprocess
+
+import inquirer
+from inquirer.themes import GreenPassion
+
+commit_type = inquirer.list_input("commit_type",choices=["verification", "design","script"])
+update_module = ""
+
+if commit_type == "design":
+    update_module = inquirer.list_input("update_module",choices=["lib", "axi","mxu","ram","others"])
+elif commit_type == "verification":
+    update_module = inquirer.list_input("update_module",choices=["checker","sequence","driver","intf","tc","env","cov","others"])
+elif commit_type == "script":
+    update_module = inquirer.list_input("update_module",choices=["git", "others"])
+
+commit_text = inquirer.text("commit_text")
+
+commit_message = f'[{commit_type}][{update_module}] {commit_text}'
+
+subprocess.run(["git",'commit','-m', commit_message])
+print(commit_message)
